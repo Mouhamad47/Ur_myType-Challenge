ea32837d3c8dc06736e9de0648bdcbc1036c0b9b
Mouhamad47 <74287155+Mouhamad47@users.noreply.github.com>
2021-11-17 15:50:02 +0200

    Fixing empty file

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.centeredView}>
+        <View style={styles.outerFirstView}>
+          <View style={styles.firstView}>
+            <View style={styles.parentSmallSquare}>
+              <View style={styles.smallSquare}></View>
+            </View>
+            <View style={styles.nameOuterView}>
+              <View style={styles.nameView}>
+                <Text style={styles.nameText}>Name</Text>
+              </View>
+            </View>
+
+            <View style={styles.parentSmallSquare}>
+              <View style={styles.smallSquare}></View>
+            </View>
+          </View>
+        </View>
+        <View style={styles.outerSecondView}>
+          <View style={styles.secondView}>
+            <View style={styles.captionView}>
+              <Text style={styles.captionText}>Balance</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f1c22',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  centeredView: {
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    width: Dimensions.get('window').width / 1.5,
+    height: Dimensions.get('window').height / 4,
+    borderStyle: 'solid',
+    borderColor: 'white',
+    borderWidth: 4,
+    borderRadius: 15,
+  },
+  firstView: {
+    flex: 3,
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    paddingRight: 30,
+    paddingLeft: 30,
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 35,
+  },
+  parentSmallSquare: {
+    height: 35
+  },
+  smallSquare: {
+    width: 38,
+    height: 38,
+    backgroundColor: '#0f1c22',
+    borderRadius: 7,
+    flex: 2
+  },
+  nameView: {
+    alignSelf: 'center',
+  },
+  nameText: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '500',
+  },
+  nameOuterView: {
+    flex: 2
+  },
+  secondView: {
+    flex: 3,
+    backgroundColor: '#f4f5fb',
+    borderTopRightRadius: 35,
+    height: '50%',
+  },
+  captionView: {
+    alignSelf: 'center',
+  },
+  captionText: {
+    textAlign: 'center',
+    paddingTop: '50%',
+    fontSize: 22,
+    fontWeight: '500'
+  },
+  outerFirstView: {
+    flex: 3,
+    backgroundColor: '#f4f5fb',
+    width: '100%',
+  },
+  outerSecondView: {
+    flex: 3,
+    backgroundColor: 'white',
+    width: '100%',
+    borderTopRightRadius: 35,
+  }
+
+
 });
